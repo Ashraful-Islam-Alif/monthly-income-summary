d8ce93040cc9ece5d495beed3c44d557dcc76f9e
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-02-17 22:23:15 +0600

    comment in code added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const balanceAfterTotalExpenses = document.getElementById('balance-after-total-expenses')
+const balanceAfterTotalExpenses = document.getElementById('balance-after-total-expenses') //global variable balance to used in different function
 function incomeExpensesCalculation() {
-    const incomeMoneyValue = incomeMoneyCalculation()
+    const incomeMoneyValue = incomeMoneyCalculation() //calling income value from function
     const foodExpenses = document.getElementById('food-expenses').value
     const rentExpenses = document.getElementById('rent-expenses').value
     const clothesExpenses = document.getElementById('clothes-expenses').value
     const totalExpenses = document.getElementById('total-expenses')
-    let totalExpensesCalculation = parseFloat(foodExpenses) + parseFloat(rentExpenses) + parseFloat(clothesExpenses)
+    let totalExpensesCalculation = parseFloat(foodExpenses) + parseFloat(rentExpenses) + parseFloat(clothesExpenses) //calculating total expenses
     const expensesMessage = document.getElementById('expenses-message')
     const negativeMessage = document.getElementById('negative-message')
+    //error checking value and negative number
     if (incomeMoneyValue > totalExpensesCalculation && incomeMoneyValue > 0 && foodExpenses > 0 && rentExpenses > 0 && clothesExpenses > 0) {
         totalExpenses.innerText = totalExpensesCalculation
         balanceAfterTotalExpenses.innerText = incomeMoneyValue - totalExpensesCalculation
@@ -29,12 +30,12 @@ function incomeMoneyCalculation() {
     const incomeMoney = document.getElementById('income-money').value
     return incomeMoney;
 }
-function expensesCalculation() { }
 
 document.getElementById('calculate-button').addEventListener('click', function () {
     incomeExpensesCalculation()
 })
 
+//save button 
 document.getElementById('save-button').addEventListener('click', function () {
     const saveInput = document.getElementById('input-save').value
     const Calculation = incomeMoneyCalculation()
@@ -44,15 +45,13 @@ document.getElementById('save-button').addEventListener('click', function () {
     const remainingBalanceText = document.getElementById('remaining-balance')
     const remainingBalance = incomeExpensesCalculation() - saveAmount
     const savingMessage = document.getElementById('saving-message')
-
+    //checking error of save percentages
     if (saveAmount < balanceAfterTotalExpenses.innerText) {
         remainingBalanceText.innerText = remainingBalance
-        savingMessage.style.display = 'none'
+        savingMessage.style.display = 'none' //error display will not shown initially
     }
     else {
-        savingMessage.style.display = 'block'
-
-
+        savingMessage.style.display = 'block' // condition not satisfy error message will show
     }
 
 })
